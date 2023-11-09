5052cbc16895b4812b67a0b62e8497a03514a1b8
Maximilian Behrendt <maximilian.behrendt@envite.de>
2023-11-09 08:07:48 +0100

    initial commit

---
+++ mavenmultimodule-core/src/main/java/info/maxbehr/mavenmultimodulecore/MavenmultimoduleCoreApplication.java
@@ -0,0 +1,13 @@
+package info.maxbehr.mavenmultimodulecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenmultimoduleCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenmultimoduleCoreApplication.class, args);
+    }
+
+}
---
+++ mavenmultimodule-core/src/test/java/info/maxbehr/mavenmultimodulecore/MavenmultimoduleCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package info.maxbehr.mavenmultimodulecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenmultimoduleCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
